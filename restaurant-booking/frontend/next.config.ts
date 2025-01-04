7e5daeb5fde193453b5377c5f13b982bed30f52a
Ashu29107 <ashutoshmishra1032003@gmail.com>
2025-01-04 21:21:11 +0530

    frontend is updated

--- restaurant-booking/frontend/next.config.ts
+++ restaurant-booking/frontend/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // Enables the App Router (Next.js 13+)
-  },
   // Add any other options here if needed
 };
 
